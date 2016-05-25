a5e8afa771a61a0cefca0088793b2a047f23b190
Nakulkomarla <nakulkolle@gmail.com>
2016-05-25 19:43:58 +0530

    Add files via upload

---
+++ assignment1.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package assignments;
+
+/**
+ * @author komarla
+ *
+ */
+public class assignment1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number1;
+		float number2;
+		number1=372;
+		number2=348.23f;//float needs to have f in end//
+		int sum = number1+(int) number2;//addition and narrowing//
+		System.out.println("sum of two numbers an int and float is:"+sum);
+		
+
+	}
+
+}
